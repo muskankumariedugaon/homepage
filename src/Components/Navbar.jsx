23002fe02c95a284a9e7edf58a47da83f143c0b5
Rinkidubey <171537099+Rinki2155@users.noreply.github.com>
2026-09-02 13:34:53 +0530

    prove

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -295,7 +295,7 @@ export default function Navbar() {
               "
             >
               About US
-            </Link>bhfbvkerbvl
+            </Link>
           </div>
 
           {/* ================================================= */}
